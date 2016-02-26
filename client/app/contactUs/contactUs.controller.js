71203efcd7af92a0bc4f8a6c66ce98808b192462
Michael Godden <smylydon@yahoo.com>
2016-02-25 21:02:05 -0800

    Created contact form, hooked it up to the backend. #19.

--- client/app/contactUs/contactUs.controller.js
+++ client/app/contactUs/contactUs.controller.js
@@ -7,9 +7,34 @@
 
 
 	 /*@ngInject*/
-	function ContactUsCtrl ($scope) {
+	function ContactUsCtrl ($scope, Restangular) {
 		var vm = this;
+		vm.customerName = '';
+		vm.message = '';
+		vm.emailAddress = '';
+		vm.subject = '';
 
+		vm.submit = function($event) {
+			//$event.stopProgration();
+			console.log('submit buttton clicked');
+			var message ={};
+
+			if (vm.customerName.length > 2){
+
+			}
+
+			message.name = vm.customerName;
+			message.message = vm.message;
+			message.email = vm.emailAddress;
+			message.subject = vm.subject;
+
+			Restangular.one('comment')
+				.customPOST(message).then(function(success) {
+			  console.log('it worked:', success);
+			}).catch(function(error) {
+			  console.log('it failed:', error);
+		  });
+		}
 	}
 
 
