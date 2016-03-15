54e745b82e4b785a2c8ba782a0cbc6600737b28c
Michael Godden <smylydon@yahoo.com>
2016-03-14 21:37:20 -0700

    Added access control list on the frontend. #28, #10.
    Access control was added to the comments route. This merely
    an example to be used with other routes. This library also
    allows template level acl control. So for example only
    admin will see the upload pix button on the vehicle page.#27.

--- client/app/comments/comments.module.js
+++ client/app/comments/comments.module.js
@@ -1,27 +1,39 @@
 'use strict';
 
 angular.module('jwtfrontendApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('comments', {
-        url: '/comments/:comment_id',
-        views: {
-          'menuView': {
-            templateUrl: 'components/navbar/navbar.tpl.html',
-            controller: 'NavbarCtrl',
-            controllerAs: 'navbarCtrl'
-          },
-          'mainView': {
-            templateUrl: 'app/comments/comments.tpl.html',
-            controller: 'CommentsCtrl',
-            controllerAs: 'commentsCtrl'
-          },
-          'footerView': {
-            templateUrl: 'components/footer/footer.tpl.html',
-            controller: 'FooterCtrl',
-            controllerAs: 'footerCtrl'
-          }
-        },
-        authenticate: true
-      });
-  });
+	.config(function ($stateProvider) {
+		$stateProvider
+			.state('comments', {
+				url: '/comments/:comment_id',
+				views: {
+					'menuView': {
+						templateUrl: 'components/navbar/navbar.tpl.html',
+						controller: 'NavbarCtrl',
+						controllerAs: 'navbarCtrl'
+					},
+					'mainView': {
+						templateUrl: 'app/comments/comments.tpl.html',
+						controller: 'CommentsCtrl',
+						controllerAs: 'commentsCtrl'
+					},
+					'footerView': {
+						templateUrl: 'components/footer/footer.tpl.html',
+						controller: 'FooterCtrl',
+						controllerAs: 'footerCtrl'
+					}
+				},
+				resolve: {
+					/*@ngInject*/
+					'acl': function ($q, AclService) {
+            console.log('can:',AclService.can('Comments'));
+						if (AclService.can('Comments')) {
+              console.log('resolved well');
+							return true;
+						} else {
+              console.log('failed to resolve');
+							return $q.reject('Unauthorized');
+						}
+					}
+				}
+			});
+	});
--- client/components/loginService/login.service.js
+++ client/components/loginService/login.service.js
@@ -1,50 +1,68 @@
-(function(app) {
-    'use strict';
-    app.module('services.login',['LocalStorageModule', 'restangular'])
-      .service('LoginService', LoginService);
+(function (app) {
+	'use strict';
+	app.module('services.login', ['LocalStorageModule', 'restangular'])
+		.service('LoginService', LoginService);
 
-    /*@ngInject*/
-    function LoginService($q, localStorageService, Restangular) {
-        var isFetchingLogin = false;
-        var loginPromise = false;
+	/*@ngInject*/
+	function LoginService($q, AclService, localStorageService, Restangular) {
+		var isFetchingLogin = false;
+		var loginPromise = false;
 
-        var external = {
-            isLoggedIn: isLoggedIn,
-            login: login,
-            logout: logout
-        };
+		var external = {
+			isLoggedIn: isLoggedIn,
+			login: login,
+			logout: logout
+		};
 
-        function isLoggedIn() {
-            return !!localStorageService.get('token');
-        }
 
-        function login(credentials) {
-            if (isFetchingLogin) {
-                return loginPromise.promise;
-            }
-            loginPromise = $q.defer();
-            Restangular.one('login').customPOST(credentials).then(function(success) {
-                localStorageService.set('token', success.token);
-                setBearerToken();
-                loginPromise.resolve('login successful');
-            }, function(error) {
-                logout();
-                loginPromise.reject('login failed');
-            });
-            return loginPromise.promise;
-        }
 
-        function logout() {
-            localStorageService.set('token', null);
-            setBearerToken();
-        }
+		function isLoggedIn() {
+			return !!localStorageService.get('token');
+		}
 
-        function setBearerToken() {
-            Restangular.setDefaultHeaders({
-                Authorization: 'Bearer ' + localStorageService.get('token')
-            });
-        }
+		function login(credentials) {
+			if (isFetchingLogin) {
+				return loginPromise.promise;
+			}
+			loginPromise = $q.defer();
+			Restangular.one('login')
+				.customPOST(credentials)
+				.then(function (success) {
+					logout();
+					var authorization = ['manager'];
+					var user = {
+						roles: authorization,
+						getRoles: function () {
+							return this.roles;
+						}
+					};
+					localStorageService.set('authorization', authorization);
+					localStorageService.set('token', success.token);
+					AclService.clearUserIdentity();
+					AclService.setUserIdentity(user);
+					setBearerToken();
+					loginPromise.resolve('login successful');
+				})
+				.catch(function (error) {
+					logout();
+					loginPromise.reject('login failed');
+				});
+			return loginPromise.promise;
+		}
 
-        return external;
-    }
+		function logout() {
+			localStorageService.set('authorization', null);
+			AclService.clearUserIdentity();
+			localStorageService.set('token', null);
+			setBearerToken();
+		}
+
+		function setBearerToken() {
+			Restangular.setDefaultHeaders({
+				Authorization: 'Bearer ' + localStorageService.get('token')
+			});
+		}
+
+		return external;
+	}
 })(angular);
