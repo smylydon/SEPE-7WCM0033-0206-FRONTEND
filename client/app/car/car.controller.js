a4d1b10eff52c484c7315e295bb9cd093822565d
Michael Godden <smylydon@yahoo.com>
2016-04-06 20:05:38 -0700

    Upload images working, removed upload page. #27, #32 and #33.

--- client/app/car/car.controller.js
+++ client/app/car/car.controller.js
@@ -1,34 +1,41 @@
-(function(app) {
-  'use strict';
+(function (app) {
+	'use strict';
 
-  app.module('jwtfrontendApp')
-    .controller('CarCtrl', CarCtrl);
+	app.module('jwtfrontendApp')
+		.controller('CarCtrl', CarCtrl);
 
-  /*@ngInject*/
-  function CarCtrl($scope, $state, $stateParams, CarsService) {
-    var vm = this;
-    vm.showCar = false;
-    vm.car = null;
+	/*@ngInject*/
+	function CarCtrl($scope, $state, $stateParams, CarsModalService, CarsService) {
+		var vm = this;
+		vm.showCar = false;
+		vm.car = null;
 
-    vm.uploadImage = function () {
-      console.log('uploadImage clicked');
-    };
+		vm.uploadImage = function () {
+			console.log('uploadImage clicked');
+		};
 
-    function getCar(id){
-        CarsService.getACar(id).then(function (car) {
-            vm.car = car;
-            vm.showCar = false;
-            if (vm.car) {
-              vm.showCar = true;
-            }
-            console.log('car:', car);
-        }, function (error) {
-          vm.showCar = false;
-            console.log(error);
-        });
-    }
+		function getCar(id) {
+			CarsService.getACar(id)
+				.then(function (car) {
+					vm.car = car;
+					vm.showCar = false;
+					if (vm.car) {
+						vm.showCar = true;
+					}
+					console.log('car:', car);
+				}, function (error) {
+					vm.showCar = false;
+					console.log(error);
+				});
+		}
 
-    getCar($stateParams.car_id); //jshint ignore:line
-  }
+		vm.editCar = function ($event) {
+			if (vm.car) {
+				CarsModalService.showModal(vm.car);
+			}
+		};
+
+		getCar($stateParams.car_id); //jshint ignore:line
+	}
 
 })(angular);
